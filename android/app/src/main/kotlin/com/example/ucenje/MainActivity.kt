eca8b9a55d28527d7112b1613a971834ed1780e5
petarjukic <jukicpetar2@gmail.com>
2022-05-17 22:27:29 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ucenje/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ucenje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Questionnaire());
+}
+
+class Questionnaire extends StatelessWidget {
+  const Questionnaire({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
